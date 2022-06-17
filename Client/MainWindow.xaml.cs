d6054a1bafa77602a8df34b3d6985e3b29890ad8
muhammettfirat <negrumaquila@gmail.com>
2022-06-17 15:24:38 +0300

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const int portno = 38000;
+        public Thread kanal;
+        private int kx = 1;
+        public MainWindow()
+        {
+           
+       
+        InitializeComponent();
+        }
+       delegate void clientdg(string datax);
+        public int i=1;
+        TcpListener tcplisten;
+        public void Client_Listen()
+        {
+            tcplisten = new TcpListener(portno);
+            try
+            {
+                while (true)
+                {
+                    tcplisten.Start();
+                    Socket socket1=tcplisten.AcceptSocket();
+                    Byte[] okunan = new Byte[1024];
+                    int boyut = socket1.Receive(okunan, okunan.Length, 0);
+                    string dataokunan = System.Text.Encoding.Default.GetString(okunan);
+                    string gidenveri = "Client :Mesaj Alınmıştır";
+                    Byte[] gidenbyte=System.Text.Encoding.Default.GetBytes(gidenveri.ToCharArray());
+                    socket1.Send(gidenbyte, gidenbyte.Length, 0);
+                    i++;
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Veri Aktarım Hatası");
+
+            }
+
+        }
+        
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TcpClient tcpcln = new TcpClient("192.168.1.188", 28015);
+                NetworkStream netws = tcpcln.GetStream();
+                NetworkStream netws1 = tcpcln.GetStream();
+                if (netws.CanWrite || netws1.CanWrite)
+                {
+                    Byte[] giden = Encoding.Default.GetBytes("Client>>" + btnLamba.Content);
+                    Byte[] giden1 = Encoding.Default.GetBytes("Client>>" + btnLamba.Content);
+                    netws.Write(giden, 0, giden.Length);
+                    netws1.Write(giden1, 0, giden1.Length);
+
+                }
+                else
+                {
+                    tcpcln.Close();
+                    return;
+                }
+                if (netws.CanRead || netws1.CanRead)
+                {
+                    byte[] oku1 = new byte[tcpcln.ReceiveBufferSize];
+                    byte[] oku2 = new byte[tcpcln.ReceiveBufferSize];
+                    netws.Read(oku1, 0, (int)tcpcln.ReceiveBufferSize);
+                    netws1.Read(oku2, 0, (int)tcpcln.ReceiveBufferSize);
+                    string gelentext = Encoding.Default.GetString(oku1);
+                    string gelenstate = Encoding.Default.GetString(oku2);
+                    tcpcln.Close();
+                    btnText.Content = gelentext;
+                    btnLamba.Content = gelenstate;
+
+                }
+                else
+                {
+                    tcpcln.Close();
+                    return;
+                }
+                kx++;
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Veri gönderme Hatası");
+            }
+        }
+
+        private void btnLamba_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TcpClient tcpcln = new TcpClient("192.168.1.188", 28015);
+                NetworkStream netws = tcpcln.GetStream();
+                NetworkStream netws1 = tcpcln.GetStream();
+                if (netws.CanWrite || netws1.CanWrite)
+                {
+                    Byte[] giden = Encoding.Default.GetBytes("Client>>" + btnLamba.Content);
+                    Byte[] giden1 = Encoding.Default.GetBytes("Client>>" + btnLamba.Content);
+                    netws.Write(giden, 0, giden.Length);
+                    netws1.Write(giden1, 0, giden1.Length);
+
+                }
+                else
+                {
+                    tcpcln.Close();
+                    return;
+                }
+                if (netws.CanRead || netws1.CanRead)
+                {
+                    byte[] oku1 = new byte[tcpcln.ReceiveBufferSize];
+                    byte[] oku2 = new byte[tcpcln.ReceiveBufferSize];
+                    netws.Read(oku1, 0, (int)tcpcln.ReceiveBufferSize);
+                    netws1.Read(oku2, 0, (int)tcpcln.ReceiveBufferSize);
+                    string gelentext = Encoding.Default.GetString(oku1);
+                    string gelenstate = Encoding.Default.GetString(oku2);
+                    tcpcln.Close();
+                    btnText.Content = gelentext;
+                    btnLamba.Content = gelenstate;
+
+                }
+                else
+                {
+                    tcpcln.Close();
+                    return;
+                }
+                kx++;
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Veri gönderme Hatası");
+            }
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int kx = 1;
+        TcpClient tcpcln;
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbl.Background = Brushes.Red;
+            SWT_Listen();
+        }
+        public const int portno = 28015;
+        public Thread kanal;
+        delegate void STD(string datax);
+        public int i = 1;
+        Socket socket1;
+        public void SWT_Listen()
+        {
+            TcpListener tcpListener = new TcpListener(portno);
+            try
+            {
+                while (true)
+                {
+                    tcpListener.Start();
+                    socket1 = tcpListener.AcceptSocket();
+                    Byte[] okunan = new Byte[1024];
+                    int boyut = socket1.Receive(okunan, okunan.Length, 0);
+                    string dataokunan = System.Text.Encoding.Default.GetString(okunan);
+                    string gidenveri = "Server Mesajınız Alınmıştır";
+                    Byte[] gidenByte = System.Text.Encoding.Default.GetBytes(gidenveri.ToCharArray());
+                    socket1.Send(gidenByte, gidenByte.Length, 0);
+                    i++;
+                  
+
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Tcp Dinleme Hatası");
+            }
+        }
+       
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+          string state;
+            if (lbl.Background == Brushes.Green)
+            {
+                lbl.Background = Brushes.Red;
+                lbl.Content = "Kapalı";
+                state = "Kapalı";
+            }
+            else if (lbl.Background == Brushes.Red)
+            {
+                lbl.Background = Brushes.Green;
+                lbl.Content = "Açık";
+                state = "Açık";
+            }
+            kanal = new Thread(SWT_Listen);
+            kanal.Priority = ThreadPriority.Normal;
+            kanal.IsBackground = true;
+            kanal.Start();
+            lblListen.Content = "Dinlemede";
+            try
+            {
+                tcpcln = new TcpClient("192.168.1.188", 38000);
+                NetworkStream netws = tcpcln.GetStream();
+                NetworkStream netws1 = tcpcln.GetStream();
+                if (netws.CanWrite || netws1.CanWrite)
+                {
+                    Byte[] gidenText = Encoding.Default.GetBytes("Server>>" + txtalan.Text);
+                    Byte[] gidenState = Encoding.Default.GetBytes("Server>>" + lbl.Content);
+
+
+                }
+                else
+                {
+                    tcpcln.Close();
+                    return;
+                }
+                kx++;
+                if (netws.CanRead || netws1.CanRead)
+                {
+                    Byte[] gelenText = Encoding.Default.GetBytes("Server>>" + txtalan.Text);
+                    Byte[] gelenState = Encoding.Default.GetBytes($"Server>> {lbl.Content}");
+                    Byte[] oku1 = new byte[tcpcln.ReceiveBufferSize];
+                    Byte[] oku2 = new byte[tcpcln.ReceiveBufferSize];
+                    string gelentext = Encoding.Default.GetString(oku1);
+                    string gelenstate = Encoding.Default.GetString(oku2);
+                    tcpcln.Close();
+                    lbl.Content = gelentext;
+                    lbl.Content = gelenstate;
+
+
+                    netws.Read(gelenText);
+                    netws1.Read(gelenState);
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Veri gönderim hatası");
+            }
+
+        }
+       
+        private void btntext_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            txtalan.Text = rnd.Next(100).ToString();
+            kanal = new Thread(SWT_Listen);
+            kanal.Priority = ThreadPriority.Normal;
+            kanal.IsBackground = true;
+            kanal.Start();
+            lblListen.Content = "Dinlemede";
+            try
+            {
+                tcpcln = new TcpClient("192.168.1.188", 38000);
+                NetworkStream netws = tcpcln.GetStream();
+                NetworkStream netws1 = tcpcln.GetStream();
+                if (netws.CanWrite || netws1.CanWrite)
+                {
+                    Byte[] gidenText = Encoding.Default.GetBytes("Server>>" + txtalan.Text);
+                    Byte[] gidenState = Encoding.Default.GetBytes("Server>>" + lbl.Content);
+
+
+                }
+                else
+                {
+                    tcpcln.Close();
+                    return;
+                }
+                kx++;
+                if (netws.CanRead || netws1.CanRead)
+                {
+                    Byte[] gelenText = Encoding.Default.GetBytes("Server>>" + txtalan.Text);
+                    Byte[] gelenState = Encoding.Default.GetBytes($"Server>> {lbl.Content}");
+                    Byte[] oku1 = new byte[tcpcln.ReceiveBufferSize];
+                    Byte[] oku2 = new byte[tcpcln.ReceiveBufferSize];
+                    string gelentext = Encoding.Default.GetString(oku1);
+                    string gelenstate = Encoding.Default.GetString(oku2);
+                    tcpcln.Close();
+                    lbl.Content = gelentext;
+                    lbl.Content = gelenstate;
+
+
+                    netws.Read(gelenText);
+                    netws1.Read(gelenState);
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Veri gönderim hatası");
+            }
+        }
+     
+    }
+}
